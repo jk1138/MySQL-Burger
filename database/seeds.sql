0307ef741536c17d558ca53f4b00fec594f1d04d
jyk <47995007+jk1138@users.noreply.github.com>
2019-10-03 08:11:38 -0400

    update npm init, and create db

---
+++ database/seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Do Brussel Burger (comes with Brussels sprouts)", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Burger a la Mode (comes with ice cream, not on top)", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Ruta-Bag-A Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("It's Only Sourdough Burger (But I Like It)", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("50 Ways to Leave Your Guava Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Summer Thyme Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Peas and Thank You Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("I Am Mad About Saffron (made with no saffron)", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Yes I Cayenne Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Onion Burger - Grilled....To Death!", false);
