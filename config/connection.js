1f80af7a7ae8b2bc152fd53b6496bd73e16fe296
jyk <47995007+jk1138@users.noreply.github.com>
2019-10-08 20:43:25 -0400

    connection issues and changed host along with sql alter user password script

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,16 @@
-/*
-Make the connection to the database and export to be used by the O.R.M.
-*/
 var mysql = require('mysql');
-var connection;
-​
+
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
     host: 'localhost',
+    port: 3306,
     user: 'root',
-    password: 'rootroot',
+    password: "password",
     database: 'burgers_db'
     });
 };
-​
 connection.connect(function (err) {
   if (err) {
     console.error('error connecting: ' + err.stack);
@@ -22,5 +18,4 @@ connection.connect(function (err) {
   }
   console.log('connected as id ' + connection.threadId);
 });
-​
 module.exports = connection;
